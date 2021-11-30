6d5b881344b1275e667c3489c82f5bbc6eddd84b
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-11-30 23:29:10 +0100

    Day 01 preparation

---
+++ src/day01.php
@@ -0,0 +1,17 @@
+<?php
+
+$input = "
+";
+
+$data = array_map('intval', preg_split("/\r?\n/", trim($input)));
+
+function solvePart1($data) {
+  return null;
+}
+
+function solvePart2($data) {
+  return -1;
+}
+
+echo "Solution 1: " . solvePart1($data) . "\n";
+echo "Solution 2: " . solvePart2($data) . "\n";
