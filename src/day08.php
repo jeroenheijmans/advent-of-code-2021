04cf2f725175cec94c311fdf6409ad8ccd7e5a54
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-07 06:36:07 +0100

    Day 08 preparation

---
+++ src/day08.php
@@ -0,0 +1,18 @@
+<?php
+
+$input = "";
+
+$data = array_map('intval', explode(",", trim($input)));
+
+function solvePart1($data) {
+  $answer = -1;
+  return $answer;
+}
+
+function solvePart2($data) {
+  $answer = -1;
+  return $answer;
+}
+
+echo "Solution 1: " . solvePart1($data) . "\n";
+echo "Solution 2: " . solvePart2($data) . "\n";
