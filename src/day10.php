737b56d94cb9df5b26e531c7bcadf52e01c36ce2
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-10 06:37:17 +0100

    Day 10 part 02

--- src/day10.php
+++ src/day10.php
@@ -135,7 +135,7 @@ function solvePart1($lines) {
         if ($part === ">" && $popped !== "<") $error = 25137;
 
         if ($error) {
-          echo "Expected matching for $part but found $popped on line $i => $line\n";
+          // echo "Expected matching for $part but found $popped on line $i => $line\n";
           $answer += $error;
           break;
         }
@@ -154,7 +154,61 @@ function solvePart1($lines) {
 
 function solvePart2($lines) {
   $answer = 0;
-  return $answer;
+  $opens = ["(", "[", "{", "<"];
+  $closes = [")", "]", "}", ">"];
+  $scores = [];
+  $i = 0;
+
+  foreach ($lines as $line) {
+    $i++;
+    $parts = str_split($line);
+    $stop = false;
+    $stack = [];
+    $error = 0;
+
+    foreach ($parts as $part) {
+      if (in_array($part, $opens))
+      {
+        array_push($stack, $part);
+      }
+      elseif (in_array($part, $closes))
+      {
+        $popped = array_pop($stack);
+        if ($part === ")" && $popped !== "(") $error = 3;
+        if ($part === "]" && $popped !== "[") $error = 57;
+        if ($part === "}" && $popped !== "{") $error = 1197;
+        if ($part === ">" && $popped !== "<") $error = 25137;
+
+        if ($error) {
+          break;
+        }
+      }
+      else
+      {
+        throw new Error("Unexpected bracket part: " . $part);
+      }
+
+      if ($stop) break;
+    }
+
+    if (!$error) {
+      echo "Valid line $i => $line with stack " . implode("", $stack) . "\n";
+      $score = 0;
+      while ($popped = array_pop($stack)) {
+        $score *= 5;
+        if ($popped === "(") $score += 1;
+        if ($popped === "[") $score += 2;
+        if ($popped === "{") $score += 3;
+        if ($popped === "<") $score += 4;
+      }
+      array_push($scores, $score);
+    }
+  }
+
+  print_r($scores);
+  sort($scores);
+  $idx = floor(count($scores) / 2);
+  return $scores[$idx];
 }
 
 echo "Solution 1: " . solvePart1($lines) . "\n";
