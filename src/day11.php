4e1bf4b24e1f0fcd9d941ad7cc678df009d4be58
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-10 23:15:40 +0100

    Day 11 part 01 preparation

---
+++ src/day11.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Illuminate\Support\Collection;
+
+function collect($value = null)
+{
+    return new Collection($value);
+}
+
+$input = "
+";
+
+$data = array_map('intval', preg_split("/\r?\n/", trim($input)));
+
+function solvePart1($data) {
+  $answer = 0;
+  return $answer;
+}
+
+function solvePart2($data) {
+  $answer = 0;
+  return $answer;
+}
+
+echo "Solution 1: " . solvePart1($data) . "\n";
+echo "Solution 2: " . solvePart2($data) . "\n";
