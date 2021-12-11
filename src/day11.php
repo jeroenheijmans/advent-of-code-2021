0910e04e746ea3c521bbbbcd4860c0795c693ff2
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-11 07:23:04 +0100

    Day 11 part 02 (incorrect, 343 is 'too low')

--- src/day11.php
+++ src/day11.php
@@ -32,8 +32,68 @@ function solvePart1($data) {
 
   for ($step = 0; $step < 100; $step++) {
 
-    echo "\nStep $step\n----\n";
-    echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
+    // echo "\nStep $step\n----\n";
+    // echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
+
+    $flashes = new Collection();
+
+    for ($y=0; $y<10; $y++) {
+      for ($x=0; $x<10; $x++) {
+        $data[$y][$x] += 1;
+        if ($data[$y][$x] > 9) $flashes->push([$y,$x]);
+      }
+    }
+
+    $flashed = new Collection();
+    $loop = 0;
+    while (!$flashes->isEmpty()) {
+      if ($loop++ > 25) throw new Error("Infinite loop?");
+      $newFlashes = new Collection();
+
+      // echo "Flashing total " . $flashes->count() . ": " . $flashes->map(fn($f) => implode(",", $f))->implode(" | ") . "\n";
+      // echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
+
+      foreach ($flashes as $flash) {
+        $flashed->push($flash);
+
+        $y = $flash[0];
+        $x = $flash[1];
+
+        $data[$y][$x] = 0; // we flashed!
+        $answer++; // so answer increases
+
+        if ($y > 0  && $x > 0 && $data[$y-1][$x-1] !== 0) $data[$y-1][$x-1] += 1;
+        if ($y > 0  &&  true  && $data[$y-1][$x+0] !== 0) $data[$y-1][$x+0] += 1;
+        if ($y > 0  && $x < 9 && $data[$y-1][$x+1] !== 0) $data[$y-1][$x+1] += 1;
+        
+        if ( true   && $x > 0 && $data[$y+0][$x-1] !== 0) $data[$y+0][$x-1] += 1;
+        if ( true   && $x < 9 && $data[$y+0][$x+1] !== 0) $data[$y+0][$x+1] += 1;
+
+        if ($y < 9 && $x > 0  && $data[$y+1][$x-1] !== 0) $data[$y+1][$x-1] += 1;
+        if ($y < 9 &&  true   && $data[$y+1][$x+0] !== 0) $data[$y+1][$x+0] += 1;
+        if ($y < 9 && $x < 9  && $data[$y+1][$x+1] !== 0) $data[$y+1][$x+1] += 1;
+      }
+      
+      for ($y=0; $y<10; $y++) {
+        for ($x=0; $x<10; $x++) {
+          if ($data[$y][$x] > 9) $newFlashes->push([$y,$x]);
+        }
+      }
+
+      $flashes = $newFlashes->unique();
+    }
+  }
+
+  return $answer;
+}
+
+function solvePart2($data) {
+
+  for ($step = 0; $step < 1000; $step++) {
+    $answer = 0;
+
+    // echo "\nStep $step\n----\n";
+    // echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
 
     $flashes = new Collection();
 
@@ -83,14 +143,11 @@ function solvePart1($data) {
       $flashes = $newFlashes->unique();
     }
 
+    // echo "Step $step ave $answer flashes\n";
+    if ($answer === 100) return $step;
   }
 
   return $answer;
-}
-
-function solvePart2($data) {
-  $flashes = 0;
-  return $flashes;
 }
 
 echo "Solution 1: " . solvePart1($data) . "\n";
