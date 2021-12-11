7e7fd2d12d0d5f4f409c7b3b2c97676cf80c0e76
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-11 07:18:44 +0100

    Day 11 part 01

--- src/day11.php
+++ src/day11.php
@@ -10,16 +10,16 @@ function collect($value = null)
 }
 
 $input = "
-5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526
+4341347643
+5477728451
+2322733878
+5453762556
+2718123421
+4237886115
+5631617114
+2217667227
+4236581255
+4482627641
 ";
 
 $data = array_map(
@@ -28,61 +28,64 @@ $data = array_map(
 );
 
 function solvePart1($data) {
-  $flashes = 0;
+  $answer = 0;
 
   for ($step = 0; $step < 100; $step++) {
 
-    echo implode("\n", array_map(fn($x) => implode($x), $data)) . "\n";
+    echo "\nStep $step\n----\n";
+    echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
 
     $flashes = new Collection();
 
-    for ($y=0; $y<0; $y++) {
-      for ($x=0; $x<0; $x++) {
+    for ($y=0; $y<10; $y++) {
+      for ($x=0; $x<10; $x++) {
         $data[$y][$x] += 1;
-        if ($data[$y][$x] > 8) $flashes->push([$y,$x]);
+        if ($data[$y][$x] > 9) $flashes->push([$y,$x]);
       }
     }
 
     $flashed = new Collection();
     $loop = 0;
-    while (!empty($flashes)) {
-      if ($loop++ > 10) throw new Error("Infinite loop?");
+    while (!$flashes->isEmpty()) {
+      if ($loop++ > 25) throw new Error("Infinite loop?");
       $newFlashes = new Collection();
 
+      // echo "Flashing total " . $flashes->count() . ": " . $flashes->map(fn($f) => implode(",", $f))->implode(" | ") . "\n";
+      // echo implode("\n", array_map(fn($x) => implode(array_map(fn($d) => $d > 9 ? "*" : "$d", $x)), $data)) . "\n";
+
       foreach ($flashes as $flash) {
-        echo "Flashing $flash\n";
         $flashed->push($flash);
-        
+
         $y = $flash[0];
         $x = $flash[1];
 
         $data[$y][$x] = 0; // we flashed!
-        $flashes++; // so answer increases
+        $answer++; // so answer increases
 
-        if ($y > 0 && $x > 0 && $data[$y-1][$x-1] !== 0) $data[$y-1][$x-1] += 1;
-        if ($y > 0 &&  true  && $data[$y-1][$x+0] !== 0) $data[$y-1][$x+0] += 1;
-        if ($y > 0 && $x < 9 && $data[$y-1][$x+1] !== 0) $data[$y-1][$x+1] += 1;
+        if ($y > 0  && $x > 0 && $data[$y-1][$x-1] !== 0) $data[$y-1][$x-1] += 1;
+        if ($y > 0  &&  true  && $data[$y-1][$x+0] !== 0) $data[$y-1][$x+0] += 1;
+        if ($y > 0  && $x < 9 && $data[$y-1][$x+1] !== 0) $data[$y-1][$x+1] += 1;
         
-        if ( true  && $x > 0 && $data[$y+0][$x-1] !== 0) $data[$y+0][$x-1] += 1;
-        if ( true  && $x < 9 && $data[$y+0][$x+1] !== 0) $data[$y+0][$x+1] += 1;
+        if ( true   && $x > 0 && $data[$y+0][$x-1] !== 0) $data[$y+0][$x-1] += 1;
+        if ( true   && $x < 9 && $data[$y+0][$x+1] !== 0) $data[$y+0][$x+1] += 1;
 
-        if ($y < 9 && $x > 0 && $data[$y+1][$x-1] !== 0) $data[$y+1][$x-1] += 1;
-        if ($y < 9 &&  true  && $data[$y+1][$x+0] !== 0) $data[$y+1][$x+0] += 1;
-        if ($y < 9 && $x < 9 && $data[$y+1][$x+1] !== 0) $data[$y+1][$x+1] += 1;
-
-        for ($y=0; $y<0; $y++) {
-          for ($x=0; $x<0; $x++) {
-            if ($data[$y][$x] > 8) $newFlashes->push([$y,$x]);
-          }
+        if ($y < 9 && $x > 0  && $data[$y+1][$x-1] !== 0) $data[$y+1][$x-1] += 1;
+        if ($y < 9 &&  true   && $data[$y+1][$x+0] !== 0) $data[$y+1][$x+0] += 1;
+        if ($y < 9 && $x < 9  && $data[$y+1][$x+1] !== 0) $data[$y+1][$x+1] += 1;
+      }
+      
+      for ($y=0; $y<10; $y++) {
+        for ($x=0; $x<10; $x++) {
+          if ($data[$y][$x] > 9) $newFlashes->push([$y,$x]);
         }
       }
 
-      $flashes = $newFlashes;
+      $flashes = $newFlashes->unique();
     }
 
   }
 
-  return $flashes;
+  return $answer;
 }
 
 function solvePart2($data) {
