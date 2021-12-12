129413aafbd961ae5bf204b6adb37e1e0d542275
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-12 07:25:01 +0100

    Day 12 part 02 (out of memory exception)
    The solution is correct for all samples, but does not properly run
    for the actual puzzle input. So must be made smarter, presumably?

--- src/day12.php
+++ src/day12.php
@@ -51,8 +51,6 @@ foreach ($data as $line) {
 // print_r($caves);
 
 function solvePart1($caves) {
-  $answer = 0;
-
   $loop = 0;
 
   $paths = [ ["start"] ];
@@ -86,15 +84,52 @@ function solvePart1($caves) {
 
   $paths = array_filter($paths, fn($path) => end($path) === "end");
 
-  print_r($paths);
-
   return count($paths);
 }
 
 function solvePart2($caves) {
+  $loop = 0;
+  $paths = [ ["start"] ];
 
-  $answer = 0;
-  return $answer;
+  while (true) {
+    $openPaths = [];
+    $finishedPaths = [];
+
+    foreach ($paths as $path) {
+      $place = end($path);
+
+      if ($place === "end") {
+        array_push($finishedPaths, $path);
+        continue;
+      }
+      
+      foreach ($caves[$place] as $target) {
+        if ($target === "start") continue;
+
+        if (ctype_lower($target) && $target !== "end") {
+          $visitedSmallCaves = array_filter($path, fn($x) => $x !== "start" && ctype_lower($x));
+          if (in_array($target, $path) && count(array_unique($visitedSmallCaves)) < count($visitedSmallCaves)) {
+            continue; // found a small cave already visited twice
+          }
+        }
+
+        $option = $path; // copy current path
+        array_push($option, $target); // add target to end
+        array_push($openPaths, $option); // save new path
+      }
+    }
+
+    if (empty($openPaths)) break;
+    if ($loop++ > 100) { throw new Error("Don't want to hang my solution..."); }
+
+    $paths = array_merge($finishedPaths, $openPaths);
+  }
+
+  $paths = array_filter($paths, fn($path) => end($path) === "end");
+
+  // foreach ($paths as $path) echo implode(",", $path) . "\n";
+
+  return count($paths);
 }
 
 echo "Solution 1: " . solvePart1($caves) . "\n";
