06b2fbf2ceedf7e6a9e5c99b2039e95554d30be9
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-12 07:04:34 +0100

    Day 12 part 01 (incorrect, 4597 is 'too high')
    Was suspected to be incorrect, because the final example input gave
    incorrect results too. But you've gotta try, right?!

--- src/day12.php
+++ src/day12.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once "../vendor/autoload.php";
 
 use Illuminate\Support\Collection;
 
@@ -10,20 +10,90 @@ function collect($value = null)
 }
 
 $input = "
+ma-start
+YZ-rv
+MP-rv
+vc-MP
+QD-kj
+rv-kj
+ma-rv
+YZ-zd
+UB-rv
+MP-xe
+start-MP
+zd-end
+ma-UB
+ma-MP
+UB-xe
+end-UB
+ju-MP
+ma-xe
+zd-UB
+start-xe
+YZ-end
 ";
 
 $data = preg_split("/\r?\n/", trim($input));
 
-function solvePart1($data) {
+$caves = [];
+
+foreach ($data as $line) {
+  $source = explode("-", $line)[0];
+  $target = explode("-", $line)[1];
+
+  if (!array_key_exists($source, $caves)) $caves[$source] = [];
+  if (!array_key_exists($target, $caves)) $caves[$target] = [];
+
+  if (!in_array($target, $caves[$source])) array_push($caves[$source], $target);
+  if (!in_array($source, $caves[$target])) array_push($caves[$target], $source);
+}
+
+// print_r($caves);
+
+function solvePart1($caves) {
+  $answer = 0;
+
+  $loop = 0;
+
+  $paths = [ ["start"] ];
+
+  while (true) {
+    $openPaths = [];
+    $finishedPaths = [];
+
+    foreach ($paths as $path) {
+      $place = end($path);
+
+      if ($place === "end") {
+        array_push($finishedPaths, $path);
+        continue;
+      }
+      
+      foreach ($caves[$place] as $target) {
+        if (ctype_lower($target) && in_array($target, $path)) continue;
+
+        $option = $path; // copy current path
+        array_push($option, $target); // add target to end
+        array_push($openPaths, $option); // save new path
+      }
+    }
+
+    if (empty($openPaths)) break;
+    if ($loop++ > 100) { throw new Error("Don't want to hang my solution..."); }
+
+    $paths = array_merge($finishedPaths, $openPaths);
+  }
+
+  print_r($paths);
+
+  return count($paths);
+}
+
+function solvePart2($caves) {
+
   $answer = 0;
   return $answer;
 }
 
-function solvePart2($data) {
-
-  $answer = 0;
-  return $answer;
-}
-
-echo "Solution 1: " . solvePart1($data) . "\n";
-echo "Solution 2: " . solvePart2($data) . "\n";
+echo "Solution 1: " . solvePart1($caves) . "\n";
+echo "Solution 2: " . solvePart2($caves) . "\n";
