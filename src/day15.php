c580a13c7a6c583956c95950933ce262c1cce565
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-15 22:59:45 +0100

    Day 15 part 01 basic setup

--- src/day15.php
+++ src/day15.php
@@ -10,11 +10,63 @@ function collect($value = null)
 }
 
 $input = "
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
 ";
 
-$data = preg_split("/\r?\n/", trim($input));
+$data = array_map(fn($row) => array_map('intval', str_split($row)), preg_split("/\r?\n/", trim($input)));
 
-function solvePart1($data) {
+class Node {
+  public $key;
+
+  public function __construct(
+    public $y,
+    public $x,
+    public $risk,
+    public $neighbors = [],
+  ) {
+    $this->key = "$y,$x";
+  }
+}
+
+$size = count($data);
+$nodes = [];
+
+for ($y = 0; $y < $size; $y++) {
+  $nodes[$y] = [];
+  for ($x = 0; $x < $size; $x++) {
+    $nodes[$y][$x] = new Node($y, $x, $data[$y][$x]);
+  }
+}
+
+for ($y = 0; $y < $size; $y++) {
+  for ($x = 0; $x < $size; $x++) {
+    if ($y > 0) array_push($nodes[$y][$x]->neighbors, $nodes[$y-1][$x]);
+    if ($x > 0) array_push($nodes[$y][$x]->neighbors, $nodes[$y][$x-1]);
+    if ($y < $size-2) array_push($nodes[$y][$x]->neighbors, $nodes[$y+1][$x]);
+    if ($x < $size-2) array_push($nodes[$y][$x]->neighbors, $nodes[$y][$x+1]);
+  }
+}
+
+function solvePart1($data, $nodes) {
+  $loop = 0;
+
+  while ($loop++ < 10) {
+    // loop through all edgeNodes
+      // find minimum next step/risk
+      // loop through all neighbors of said risk that have not been fully visited
+      // remember the risk to reach each such node
+      // loop
+  }
+
   return -1;
 }
 
