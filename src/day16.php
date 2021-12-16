c9fd9006a7c6a055e197f472573605874c86a306
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-16 07:13:56 +0100

    Day 16 part 01 (partially done)

--- src/day16.php
+++ src/day16.php
@@ -9,12 +9,73 @@ function collect($value = null)
     return new Collection($value);
 }
 
-$input = "
-";
+function hexbin($in) { return decbin(hexdec($in)); }
 
-$data = preg_split("/\r?\n/", trim($input));
+$input = "D2FE28";
+
+$data = trim($input);
+
+function parseValuePacket($bits, $pos) {
+  $numberbits = "";
+
+  while (true) {
+    $start = substr($bits, $pos, 1);
+    $part = substr($bits, $pos + 1, 4);
+
+    $numberbits .= $part;
+    if ($start === "0") break;
+
+    $pos += 5;
+    if ($pos > strlen($bits)) throw new Error("Parser went beyond end...");
+  }
+
+  $number = bindec($numberbits);
+
+  return [
+    "value" => $number,
+    "newPos" => $pos
+  ];
+}
 
 function solvePart1($data) {
+  $bits = hexbin($data);
+  
+  $versionBits = substr($bits, 0, 3);
+  $typeBits = substr($bits, 3, 3);
+
+  $version = bindec($versionBits);
+  $type = bindec($typeBits);
+
+  $pos = 6;
+
+  echo "Reading $data\n";
+  echo "Reading $bits\n";
+  echo "Version $version type $type\n";
+
+  if ($type === 4) { // Literal value
+    $result = parseValuePacket($bits, $pos);
+    echo "Literal value: " . $result["value"] . "\n";
+    $pos = $result["newPos"];
+
+  } else { // Operator value
+    $lengthTypeId = substr($bits, $pos, 1);
+    $pos++;
+    $length = $lengthTypeId === "0" ? 15 : 11;
+
+    echo "Length type ID $lengthTypeId so length $length\n";
+  
+    $nr = bindec(substr($bits, $pos, $length));
+    $pos += $length;
+
+    for ($n = 0; $n < $nr; $n++) {
+      $part = substr($bits, $pos, $length);
+      echo "Read sub packet $part\n";
+
+      $pos += $length;
+      if ($pos > strlen($bits)) throw new Error("Parser went beyond end...");
+    }
+  }
+
   return -1;
 }
 
