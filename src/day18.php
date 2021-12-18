ad85b75a42869995bc630a306de2def88d0fca19
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-18 10:17:32 +0100

    Day 18 part 01 partial

--- src/day18.php
+++ src/day18.php
@@ -11,13 +11,12 @@ function collect($value = null)
 
 // $input = "3,4,3,1,2";
 $input = "
-[1,2]
-[[1,2],3]
-[9,[8,7]]
-[[1,9],[8,5]]
-[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
-[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
-[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]
+[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]
 ";
 
 $data = preg_split("/\r?\n/", trim($input));
@@ -32,11 +31,70 @@ class Tree {
   ) {
   }
 
+  public function isValue() { return $this->value !== null; }
+
+  public function isPair() { return $this->left?->isValue() && $this->right?->isValue(); }
+
+  public function explodable() {
+    if ($this->isValue()) return false;
+    echo "  left? " . $this->left->isValue() . " right? " . $this->right->isValue() . " depth: " . $this->depth . "\n";
+    return $this->left->isValue() && $this->right->isValue() && $this->depth > 4;
+  }
+
+  public function splittable() {
+    return $this->isValue() && $this->value >= 10;
+  }
+
+  public function split() {
+    if (!$this->splittable()) throw new Error("Trying to split unsplittable item");
+    // echo "Splitting myself I am now: $this\n";
+
+    $this->left = new Tree($this->depth + 1);
+    $this->right = new Tree($this->depth + 1);
+
+    $this->left->value = floor($this->value / 2);
+    $this->right->value = ceil($this->value / 2);
+
+    $this->left->parent = $this;
+    $this->right->parent = $this;
+
+    $this->value = null;
+    // echo "Just split myself I am now: $this\n";
+  }
+
+  public function deepenOne() {
+    $this->depth++;
+    $this->left?->deepenOne();
+    $this->right?->deepenOne();
+  }
+
+  public function buildLeavesList() {
+    $result = [];
+    if ($this->isValue()) {
+      array_push($result, $this);
+    } else {
+      $result = array_merge($result, $this->left?->buildLeavesList());
+      $result = array_merge($result, $this->right?->buildLeavesList());
+    }
+    return $result;
+  }
+
+  public function buildPairsList() {
+    $result = [];
+    if ($this->isPair()) {
+      array_push($result, $this);
+    } else {
+      $result = array_merge($result, $this->left?->buildPairsList() ?? []);
+      $result = array_merge($result, $this->right?->buildPairsList() ?? []);
+    }
+    return $result;
+  }
+
   public function __toString() {
     $result = "";
     if ($this->value !== null) $result .= $this->value;
     else $result .= "[" . $this->left . "," . $this->right . "]";
-    return $result;
+    return $result . "";
   }
 }
 
@@ -77,11 +135,121 @@ function parse($text) {
   return $top->left;
 }
 
-$t = parse("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]");
-echo "$t\n";
+function xpl($tree) {
+  $list = $tree->buildPairsList();
+
+  foreach ($list as $toExplode) {
+    echo "Considering $toExplode for explosion\n";
+    if ($toExplode->explodable()) {
+      $list = $tree->buildLeavesList();
+      echo "Exploding: $toExplode\n";
+      
+      // the pair's left value is added to the first regular number to the left of the exploding pair (if any)
+      $idx = array_search($toExplode->left, $list, $strict = true) - 1;
+      if (key_exists($idx, $list)) $list[$idx]->value += $toExplode->left->value;
+
+      // the pair's right value is added to the first regular number to the right of the exploding pair (if any)
+      $idx = array_search($toExplode->right, $list, $strict = true) + 1;
+      if (key_exists($idx, $list)) $list[$idx]->value += $toExplode->right->value;
+
+      // Then, the entire exploding pair is replaced with the regular number 0
+      $toExplode->left = null;
+      $toExplode->right = null;
+      $toExplode->value = 0;
+
+      return true; // idiomatic PHP, non? :P
+    }
+  }
+
+  return false;
+}
+
+function splt($tree) {
+  $list = $tree->buildLeavesList();
+
+  while (current($list)) {
+    if (current($list)->splittable()) {
+      current($list)->split();
+      return true;
+    }
+    next($list);
+  }
+
+  return false;
+}
+
+function add($a, $b) {
+  $result = new Tree(0);
+  $result->left = $a;
+  $result->right = $b;
+  $result->left->deepenOne();
+  $result->right->deepenOne();
+  return $result;
+}
+
+function reduce($tree) {
+  // echo "\nStarted with: $tree\n";
+  $loop = 0;
+  do {
+    $reduced = false;
+    if (xpl($tree)) {
+      // echo "Exploded to: $tree\n";
+      $reduced = true;
+    }
+    if (splt($tree)) {
+      // echo "Splitted to: $tree\n";
+      $reduced = true;
+    }
+    if ($loop++ > 500) throw new Error("More than 500 loops needed");
+  } while ($reduced);
+  // echo "Done reducing to: $tree\n\n";
+}
+
+// Checks for explode
+// $t = parse("[[[[[9,8],1],2],3],4]");                  $result = xpl($t); echo "$result\n$t should be\n[[[[0,9],2],3],4]\n\n";
+// $t = parse("[7,[6,[5,[4,[3,2]]]]]");                  $result = xpl($t); echo "$result\n$t should be\n[7,[6,[5,[7,0]]]]\n\n";
+// $t = parse("[[6,[5,[4,[3,2]]]],1]");                  $result = xpl($t); echo "$result\n$t should be\n[[6,[5,[7,0]]],3]\n\n";
+// $t = parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]");  $result = xpl($t); echo "$result\n$t should be\n[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]\n\n";
+// $t = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");      $result = xpl($t); echo "$result\n$t should be\n[[3,[2,[8,0]]],[9,[5,[7,0]]]]\n\n";
+
+// $a = parse("[[[[4,3],4],4],[7,[[8,4],9]]]");
+// $b = parse("[1,1]");
+
+// $curr = add($a, $b);
+// reduce($curr);
+
+// echo "$curr should be \n";
+// echo "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]\n";
+
+// echo "\n";
+// $one = parse("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]");
+// $two = parse("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]");
+// echo "  $one\n";
+// echo "+ $two\n";
+// $added = add($one, $two);
+// echo "= $added\n";
+// reduce($added);
+// echo "= $added\n";
+// echo "= [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]] (expected)\n";
+
+// $t = parse("[[[[[3,0],[5,3]],[4,4]],[5,5]],[6,6]]");
+// echo "$t\n";
+// reduce($t);
+// echo "$t\n";
 
 function solvePart1($data) {
-  return -1;
+  $nrs = array_map(fn($x) => parse($x), $data);
+  $current = $nrs[0];
+  while ($next = next($nrs)) {
+    echo "  $current\n";
+    echo "+ $next\n";
+    $current = add($current, $next);
+    echo "> $current\n";
+    echo "Depths => " . implode(", ", array_map(fn($x) => $x->depth, $current->buildLeavesList())) . "\n";
+    reduce($current);
+    echo "= $current\n\n";
+  }
+  echo "Final result: $current\n";
 }
 
 function solvePart2($data) {
