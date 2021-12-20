f50277864eae9e54cb760cfa6da002fd3049ac42
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-20 06:21:11 +0100

    Day 20 parsing done

--- src/day20.php
+++ src/day20.php
@@ -6,17 +6,58 @@ use Illuminate\Support\Collection;
 function collect($value = null) { return new Collection($value); }
 
 $input = "
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
+#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###
+.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.
+.#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....
+.#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..
+...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....
+..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
 ";
 
-$data = preg_split("/\r?\n/", trim($input));
+function bin2image($binstring) { return str_replace("1", "#", str_replace("0", ".", $binstring)); }
+function image2bin($binstring) { return str_replace("#", "1", str_replace(".", "0", $binstring)); }
+function printimg($img) {
+  foreach ($img as $line) {
+    foreach ($line as $char) {
+      echo $char === "1" ? "#" : ".";
+    }
+    echo "\n";
+  }
+}
 
-function solvePart1($scanners) {
+$data = preg_split("/\r?\n/", trim(image2bin($input)));
+
+$algo = "";
+$rest = false;
+$image = [];
+foreach ($data as $line) {
+  if (empty($line)) $rest = true;
+  else if (!$rest) {
+    $algo .= $line;
+  }
+  else {
+    $row = [];
+    foreach (str_split($line) as $char) {
+      array_push($row, $char);
+    }
+    array_push($image, $row);
+  }
+}
+
+function solvePart1($algo, $img) {
   return -1;
 }
 
-function solvePart2($data) {
+function solvePart2($algo, $img) {
   return -1;
 }
 
-echo "Solution 1: " . solvePart1($data) . "\n";
-echo "Solution 2: " . solvePart2($data) . "\n";
+echo "Solution 1: " . solvePart1($algo, $img) . "\n";
+echo "Solution 2: " . solvePart2($algo, $img) . "\n";
