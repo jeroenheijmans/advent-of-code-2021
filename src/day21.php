8e0273003f8c5c1785d7853c3a093a6e942d7b50
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-21 06:49:17 +0100

    Day 21 part 01 cleanup

--- src/day21.php
+++ src/day21.php
@@ -15,38 +15,36 @@ function solvePart1() {
   $score2 = 0;
 
   for ($round = 1; $round < 1000; $round++) {
-    echo "[Round $round] Player 1 rolls ";
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
     $score1 += $p1; 
-    echo " ending on $p1 scoring $score1\n";
+    
     if ($score1 >= 1000) break;
 
-    echo "[Round $round] Player 2 rolls ";
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
-    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    if (++$die === 101) $die = 1; $rolls++; 
     $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
     $score2 += $p2; 
-    echo " ending on $p2 scoring $score2\n";
 
-    // echo "Round $round scores are $score1 and $score2\n";
     if ($score2 >= 1000) break;
   }
 
-  $answer = min($score1, $score2) * $rolls;
-  echo "\nmin($score1, $score2) * $rolls = $answer\n";
+  return min($score1, $score2) * $rolls;
 }
 
 function solvePart2() {
+  $p1 = 4; // Sample
+  $p2 = 8; // Sample
+
   return -1;
 }
 
-echo "Solution 1: " . solvePart1($data) . "\n";
-echo "Solution 2: " . solvePart2($data) . "\n";
+echo "Solution 1: " . solvePart1() . "\n";
+echo "Solution 2: " . solvePart2() . "\n";
