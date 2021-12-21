b0529b3811aa3c058c923997afa2735ded8c6b2a
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-21 06:38:23 +0100

    Day 21 part 01

--- src/day21.php
+++ src/day21.php
@@ -5,16 +5,46 @@ require_once "../vendor/autoload.php";
 use Illuminate\Support\Collection;
 function collect($value = null) { return new Collection($value); }
 
-$input = "
-";
+function solvePart1() {
+  $p1 = 4;
+  $p2 = 5;
+  $die = 100;
+  $rolls = 0;
 
-$data = preg_split("/\r?\n/", trim($input));
+  $score1 = 0;
+  $score2 = 0;
 
-function solvePart1($scanners) {
-  return -1;
+  for ($round = 1; $round < 1000; $round++) {
+    echo "[Round $round] Player 1 rolls ";
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p1 += ($die % 10); if ($p1 > 10) $p1 -= 10;
+    $score1 += $p1; 
+    echo " ending on $p1 scoring $score1\n";
+    if ($score1 >= 1000) break;
+
+    echo "[Round $round] Player 2 rolls ";
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
+    if (++$die === 101) $die = 1; $rolls++; echo " $die ";
+    $p2 += ($die % 10); if ($p2 > 10) $p2 -= 10;
+    $score2 += $p2; 
+    echo " ending on $p2 scoring $score2\n";
+
+    // echo "Round $round scores are $score1 and $score2\n";
+    if ($score2 >= 1000) break;
+  }
+
+  $answer = min($score1, $score2) * $rolls;
+  echo "\nmin($score1, $score2) * $rolls = $answer\n";
 }
 
-function solvePart2($data) {
+function solvePart2() {
   return -1;
 }
 
