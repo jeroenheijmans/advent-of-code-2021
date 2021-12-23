fb5b8a418a3b9b626531150540e1c941b67da5db
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-23 07:11:27 +0100

    Day 23 part 01 (incorrect: 18257 is 'too high')

--- src/day23.php
+++ src/day23.php
@@ -6,14 +6,80 @@ use Illuminate\Support\Collection;
 function collect($value = null) { return new Collection($value); }
 
 $input = "
+#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
 ";
 
-$data = preg_split("/\r?\n/", trim($input));
+$data = array_map(fn($x) => str_split($x), preg_split("/\r?\n/", trim($input)));
+
+class Pawn {
+  function __construct(
+    public $color,
+  ) {
+    if ($color === "A") $this->step = 1;
+    if ($color === "B") $this->step = 10;
+    if ($color === "C") $this->step = 100;
+    if ($color === "D") $this->step = 1000;
+  }
+}
+
+class Location {
+  function __construct(
+    public $coords,
+    public $targetFor,
+    public $isStop = false,
+    public $pawn = null,
+  ) {}
+}
+
+function printlevel($locations) {
+  for ($y = 0; $y < 5; $y++) {
+    for ($x = 0; $x < 13; $x++) {
+      $location = $locations->get("$y,$x");
+      if ($location !== null) echo $location->pawn?->color ?? "·";
+      else echo " ";
+    }
+    echo "\n";
+  }
+}
 
 function solvePart1($data) {
-  /*
-    Manual attempt:
+  $locations = new Collection();
+  for ($y = 1; $y < 4; $y++) {
+    for ($x = 1; $x < 12; $x++) {
+      if (!array_key_exists($x, $data[$y])) continue;
+      if (preg_match("/[.ABCD]/", $data[$y][$x])) {
+        $targetFor = ($x === 3 && $y > 1) ? "A" : null;
+        $targetFor = ($x === 5 && $y > 1) ? "B" : null;
+        $targetFor = ($x === 7 && $y > 1) ? "C" : null;
+        $targetFor = ($x === 9 && $y > 1) ? "D" : null;
 
+        $isStop = $y > 1 && ($x === 3 || $x === 5 || $x === 7 || $x === 9);
+
+        $pawn = preg_match("/[ABCD]/", $data[$y][$x])
+          ? new Pawn($data[$y][$x])
+          : null;
+
+        $locations->put(
+          "$y,$x",
+          new Location(
+            [$y, $x],
+            $targetFor,
+            $isStop,
+            $pawn
+          )
+        );
+      }
+    }
+  }
+
+  printlevel($locations);
+
+  /*
+  Okay once more manually:
 #############
 #...........#
 ###A#C#B#A###
@@ -27,56 +93,44 @@ function solvePart1($data) {
   #D#D#B#C#
   #########
 
-311
+111
 #############
-#AA.......c.#
-### #C#B# ###
-  #D#D#B# #
+#AA.......BB#
+### #C# # ###
+  #D#D# #C#
   #########
 
-10311
+1111
 #############
-#AA.......c.#
-### #C#B# ###
-  # #D#B#D#
+#AA.......BB#
+### # #C# ###
+  #D#D#C# #
   #########
 
-10511
+18111
 #############
-#AA.C.....c.#
-### # #B# ###
-  # #D#B#D#
+#AA.......BB#
+### # #C#D###
+  # # #C#D#
   #########
 
-17511
+18251
 #############
-#AA.C.....c.#
-### # #B#D###
-  # # #B#D#
-  #########
-
-17611
-#############
-#AA.C.....c.#
-### #B# #D###
-  # #B# #D#
-  #########
-
-18511
-#############
-#AA. ..... .#
+#AA.......  #
 ### #B#C#D###
   # #B#C#D#
   #########
 
-18517
+18257
 #############
-#  . ..... .#
+#  .......  #
 ###A#B#C#D###
   #A#B#C#D#
   #########
   */
-  return 18517;
+
+  
+  return 18257;
 }
 
 function solvePart2($data) {
