c44c362f168b81302c78931f73febf1e8766c8d3
Jeroen Heijmans <github@jeroenheijmans.nl>
2021-12-27 23:22:31 +0100

    Day 23 part 01 reboot

--- src/day23.php
+++ src/day23.php
@@ -13,7 +13,8 @@ $input = "
   #########
 ";
 
-$data = array_map(fn($x) => str_split($x), preg_split("/\r?\n/", trim($input)));
+
+$data = array_map(fn($x) => str_split($x), explode(PHP_EOL, trim($input)));
 
 class Location {
   function __construct(
@@ -21,41 +22,40 @@ class Location {
     public $x,
     public $pawn = ".",
   ) {
-
+    $this->connections = new Collection();
   }
 }
 
-$level = new Collection();
-for ($y = 1; $y < 4; $y++) {
-  for ($x = 1; $x < 12; $x++) {
-    if (!array_key_exists($x, $data[$y])) continue;
-    if ($data[$y][$x] === ".") $level->push(new Location($y, $x));
-    if (ctype_alpha($data[$y][$x])) $level->push(new Location($y, $x, $data[$y][$x]));
+$locations = new Collection();
+
+for ($x = 1; $x < 12; $x++) {
+  $locations->put("1;$x", new Location($y=0, $x=$x));
+}
+for ($y = 2; $y < 4; $y++) {
+  for ($x = 3; $x < 11; $x = $x + 2) {
+    $locations->put("$y;$x", new Location($y, $x, $data[$y][$x]));
   }
 }
 
-function printlevel($level) {
-  for ($y = 0; $y < 5; $y++) {
-    echo "\n";
-    for ($x = 0; $x < 13; $x++) {
-      if ($y > 2 && $x < 2) { echo " "; continue; }
-      if ($y > 2 && $x > 10) { echo " "; continue; }
-      $loc = $level->filter(fn($i) => $i->x === $x && $i->y === $y)->first();
-      echo $loc ? $loc->pawn : "#";
+function show($locations) {
+  for ($y = 1; $y < 4; $y++) {
+    for ($x = 0; $x < 12; $x++) {
+      if ($locations->has("$y;$x")) echo $locations->get("$y;$x")->pawn;
+      else echo " ";
     }
+    echo "\n";
   }
-  echo "\n";
 }
 
-printlevel($level);
+show($locations);
 
-function solvePart1($level) {
+function solvePart1($locations) {
   return -1;
 }
 
-function solvePart2($level) {
+function solvePart2($locations) {
   return -1;
 }
 
-echo "Solution 1: " . solvePart1($level) . "\n";
-echo "Solution 2: " . solvePart2($level) . "\n";
+echo "Solution 1: " . solvePart1($locations) . "\n";
+echo "Solution 2: " . solvePart2($locations) . "\n";
