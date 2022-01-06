93092961f227a9ac016778c8364fa8a02dd3b9fc
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-06 18:50:22 +0100

    Day 24 part 01: ternary to if/else

--- src/day24.cs
+++ src/day24.cs
@@ -7,93 +7,163 @@ static void RunProgram() {
   // Digit 01
   w = nextDigit();
   x = (z % 26) + 13;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 6) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 6) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 6) * x);
+  }
 
   // Digit 02
   w = nextDigit();
   x = (z % 26) + 15;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 7) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 7) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 7) * x);
+  }
 
   // Digit 03
   w = nextDigit();
   x = (z % 26) + 15;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  }
 
   // Digit 04
   w = nextDigit();
   x = (z % 26) + 11;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 2) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 2) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 2) * x);
+  }
 
   // Digit 05
   w = nextDigit();
   x = (z % 26) + -7;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 15) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 15) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 15) * x);
+  }
 
   // Digit 06
   w = nextDigit();
   x = (z % 26) + 10;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 8) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 8) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 8) * x);
+  }
 
   // Digit 07
   w = nextDigit();
   x = (z % 26) + 10;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 1) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 1) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 1) * x);
+  }
 
   // Digit 08
   w = nextDigit();
   x = (z % 26) + -5;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  }
 
   // Digit 09
   w = nextDigit();
   x = (z % 26) + 15;
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  }
 
   // Digit 10
   w = nextDigit();
   x = (z % 26) + -3;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 3) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 3) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 3) * x);
+  }
 
   // Digit 11
   w = nextDigit();
   x = (z % 26) + 0;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 5) * x);
+  }
 
   // Digit 12
   w = nextDigit();
   x = (z % 26) + -5;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 11) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 11) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 11) * x);
+  }
 
   // Digit 13
   w = nextDigit();
   x = (z % 26) + -9;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 12) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 12) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 12) * x);
+  }
 
   // Digit 14
   w = nextDigit();
   x = (z % 26) + 0;
   z = z / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  x = x == w ? 0 : 1;
-  z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  if (x == w) {
+    x = 0;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  } else {
+    x = 1;
+    z = (z * ((25 * x) + 1)) + ((w + 10) * x);
+  }
 }
 
 Console.WriteLine("Working!");
