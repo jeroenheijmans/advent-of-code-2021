2a965d430f57a6ed22ecb7c2d19a4776623f90a7
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-06 20:58:03 +0100

    Day 24 part 01: working and reduced

--- src/day24.cs
+++ src/day24.cs
@@ -1,72 +1,75 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
-static void RunProgram() {
-  long x, output = 0;
+static long RunProgram(string digits) {
+  long input, x, output = 0;
 
-  // Digit 01
+  input = digits[1-1] - '0'; // Digit 01
   x = (output % 26) + 13;
-  if (x != input()) output = (output * 26) + (input + 6);
+  if (x != input) output = (output * 26) + (input + 6);
 
-  // Digit 02
+  input = digits[2-1] - '0'; // Digit 02
   x = (output % 26) + 15;
-  if (x != input()) output = (output * 26) + (input + 7);
+  if (x != input) output = (output * 26) + (input + 7);
 
-  // Digit 03
+  input = digits[3-1] - '0'; // Digit 03
   x = (output % 26) + 15;
-  if (x != input()) output = (output * 26) + (input + 10);
+  if (x != input) output = (output * 26) + (input + 10);
 
-  // Digit 04
+  input = digits[4-1] - '0'; // Digit 04
   x = (output % 26) + 11;
-  if (x != input()) output = (output * 26) + (input + 2);
+  if (x != input) output = (output * 26) + (input + 2);
 
-  // Digit 05
+  input = digits[5-1] - '0'; // Digit 05
   x = (output % 26) + -7;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 15);
+  if (x != input) output = (output * 26) + (input + 15);
 
-  // Digit 06
+  input = digits[6-1] - '0'; // Digit 06
   x = (output % 26) + 10;
-  if (x != input()) output = (output * 26) + (input + 8);
+  if (x != input) output = (output * 26) + (input + 8);
 
-  // Digit 07
+  input = digits[7-1] - '0'; // Digit 07
   x = (output % 26) + 10;
-  if (x != input()) output = (output * 26) + (input + 1);
+  if (x != input) output = (output * 26) + (input + 1);
 
-  // Digit 08
+  input = digits[8-1] - '0'; // Digit 08
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 10);
+  if (x != input) output = (output * 26) + (input + 10);
 
-  // Digit 09
+  input = digits[9-1] - '0'; // Digit 09
   x = (output % 26) + 15;
-  if (x != input()) output = (output * 26) + (input + 5);
+  if (x != input) output = (output * 26) + (input + 5);
 
-  // Digit 10
+  input = digits[10-1] - '0'; // Digit 10
   x = (output % 26) + -3;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 3);
+  if (x != input) output = (output * 26) + (input + 3);
 
-  // Digit 11
+  input = digits[11-1] - '0'; // Digit 11
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 5);
+  if (x != input) output = (output * 26) + (input + 5);
 
-  // Digit 12
+  input = digits[12-1] - '0'; // Digit 12
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 11);
+  if (x != input) output = (output * 26) + (input + 11);
 
-  // Digit 13
+  input = digits[13-1] - '0'; // Digit 13
   x = (output % 26) + -9;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 12);
+  if (x != input) output = (output * 26) + (input + 12);
 
-  // Digit 14
+  input = digits[14-1] - '0'; // Digit 14
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x != input()) output = (output * 26) + (input + 10);
+  if (x != input) output = (output * 26) + (input + 10);
+
+  return output;
 }
 
-Console.WriteLine("Working!");
-RunProgram();
+Console.WriteLine("Starting day 24 in csharp...");
+Console.WriteLine($"13579246899999 gives result {RunProgram("13579246899999")}");
