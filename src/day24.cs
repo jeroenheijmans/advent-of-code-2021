b7511b94b8b49b600e4de5a839326fd6d95d6faf
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-06 18:36:10 +0100

    Day 24 part 01: literal translation of MONAD to C#

--- src/day24.cs
+++ src/day24.cs
@@ -2,7 +2,273 @@ using System;
 using System.Linq;
 
 static void RunProgram() {
-  Console.WriteLine("Running program...");
+  long w, x, y, z;
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 13; // add x 13
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 6; // add y 6
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 15; // add x 15
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 7; // add y 7
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 15; // add x 15
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 10; // add y 10
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 11; // add x 11
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 2; // add y 2
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + -7; // add x -7
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 15; // add y 15
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 10; // add x 10
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 8; // add y 8
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 10; // add x 10
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 1; // add y 1
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + -5; // add x -5
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 10; // add y 10
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 1; // div z 1
+  x = x + 15; // add x 15
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 5; // add y 5
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + -3; // add x -3
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 3; // add y 3
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + 0; // add x 0
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 5; // add y 5
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + -5; // add x -5
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 11; // add y 11
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + -9; // add x -9
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 12; // add y 12
+  y = y * x; // mul y x
+  z = z + y; // add z y
+  
+  w = nextDigit(); // inp w
+  x = 0; // mul x 0
+  x = x + z; // add x z
+  x = x % 26; // mod x 26
+  z = z / 26; // div z 26
+  x = x + 0; // add x 0
+  x = x == w ? 1 : 0; // eql x w
+  x = x == 0 ? 1 : 0; // eql x 0
+  y = 0; // mul y 0
+  y = y + 25; // add y 25
+  y = y * x; // mul y x
+  y = y + 1; // add y 1
+  z = z * y; // mul z y
+  y = 0; // mul y 0
+  y = y + w; // add y w
+  y = y + 10; // add y 10
+  y = y * x; // mul y x
+  z = z + y; // add z y
 }
 
 Console.WriteLine("Working!");
