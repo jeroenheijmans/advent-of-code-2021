3f9278be82eb55c61aa15c99811af951c0b7744c
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-06 18:56:20 +0100

    Day 24 part 01: shortening each block

--- src/day24.cs
+++ src/day24.cs
@@ -4,166 +4,68 @@ using System.Linq;
 static void RunProgram() {
   long input, x, y, output;
 
-  // Digit 01
-  input = nextDigit();
+  input = nextDigit(); // Digit 01
   x = (output % 26) + 13;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 6);
-  }
+  if (x != input) output = (output * 26) + (input + 6);
 
-  // Digit 02
-  input = nextDigit();
+  input = nextDigit(); // Digit 02
   x = (output % 26) + 15;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 7);
-  }
+  if (x != input) output = (output * 26) + (input + 7);
 
-  // Digit 03
-  input = nextDigit();
+  input = nextDigit(); // Digit 03
   x = (output % 26) + 15;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 10);
-  }
+  if (x != input) output = (output * 26) + (input + 10);
 
-  // Digit 04
-  input = nextDigit();
+  input = nextDigit(); // Digit 04
   x = (output % 26) + 11;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 2);
-  }
+  if (x != input) output = (output * 26) + (input + 2);
 
-  // Digit 05
-  input = nextDigit();
+  input = nextDigit(); // Digit 05
   x = (output % 26) + -7;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 15);
-  }
+  if (x != input) output = (output * 26) + (input + 15);
 
-  // Digit 06
-  input = nextDigit();
+  input = nextDigit(); // Digit 06
   x = (output % 26) + 10;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 8);
-  }
+  if (x != input) output = (output * 26) + (input + 8);
 
-  // Digit 07
-  input = nextDigit();
+  input = nextDigit(); // Digit 07
   x = (output % 26) + 10;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 1);
-  }
+  if (x != input) output = (output * 26) + (input + 1);
 
-  // Digit 08
-  input = nextDigit();
+  input = nextDigit(); // Digit 08
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 10);
-  }
+  if (x != input) output = (output * 26) + (input + 10);
 
-  // Digit 09
-  input = nextDigit();
+  input = nextDigit(); // Digit 09
   x = (output % 26) + 15;
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 5);
-  }
+  if (x != input) output = (output * 26) + (input + 5);
 
-  // Digit 10
-  input = nextDigit();
+  input = nextDigit(); // Digit 10
   x = (output % 26) + -3;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 3);
-  }
+  if (x != input) output = (output * 26) + (input + 3);
 
-  // Digit 11
-  input = nextDigit();
+  input = nextDigit(); // Digit 11
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 5);
-  }
+  if (x != input) output = (output * 26) + (input + 5);
 
-  // Digit 12
-  input = nextDigit();
+  input = nextDigit(); // Digit 12
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 11);
-  }
+  if (x != input) output = (output * 26) + (input + 11);
 
-  // Digit 13
-  input = nextDigit();
+  input = nextDigit(); // Digit 13
   x = (output % 26) + -9;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 12);
-  }
+  if (x != input) output = (output * 26) + (input + 12);
 
-  // Digit 14
-  input = nextDigit();
+  input = nextDigit(); // Digit 14
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
-  if (x == input) {
-    // x = 0 so nothing happens
-    output = output;
-  } else {
-    // x = 1
-    output = (output * 26) + (input + 10);
-  }
+  if (x != input) output = (output * 26) + (input + 10);
 }
 
 Console.WriteLine("Working!");
