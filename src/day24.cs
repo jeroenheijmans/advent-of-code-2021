851ee4eb0336208b53b6ef6693b96e5c0fd39f03
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-06 18:54:56 +0100

    Day 24 part 01: getting rid of x

--- src/day24.cs
+++ src/day24.cs
@@ -8,44 +8,44 @@ static void RunProgram() {
   input = nextDigit();
   x = (output % 26) + 13;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 6) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 6) * x);
+    // x = 1
+    output = (output * 26) + (input + 6);
   }
 
   // Digit 02
   input = nextDigit();
   x = (output % 26) + 15;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 7) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 7) * x);
+    // x = 1
+    output = (output * 26) + (input + 7);
   }
 
   // Digit 03
   input = nextDigit();
   x = (output % 26) + 15;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 1
+    output = (output * 26) + (input + 10);
   }
 
   // Digit 04
   input = nextDigit();
   x = (output % 26) + 11;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 2) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 2) * x);
+    // x = 1
+    output = (output * 26) + (input + 2);
   }
 
   // Digit 05
@@ -53,33 +53,33 @@ static void RunProgram() {
   x = (output % 26) + -7;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 15) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 15) * x);
+    // x = 1
+    output = (output * 26) + (input + 15);
   }
 
   // Digit 06
   input = nextDigit();
   x = (output % 26) + 10;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 8) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 8) * x);
+    // x = 1
+    output = (output * 26) + (input + 8);
   }
 
   // Digit 07
   input = nextDigit();
   x = (output % 26) + 10;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 1) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 1) * x);
+    // x = 1
+    output = (output * 26) + (input + 1);
   }
 
   // Digit 08
@@ -87,22 +87,22 @@ static void RunProgram() {
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 1
+    output = (output * 26) + (input + 10);
   }
 
   // Digit 09
   input = nextDigit();
   x = (output % 26) + 15;
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 5) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 5) * x);
+    // x = 1
+    output = (output * 26) + (input + 5);
   }
 
   // Digit 10
@@ -110,11 +110,11 @@ static void RunProgram() {
   x = (output % 26) + -3;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 3) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 3) * x);
+    // x = 1
+    output = (output * 26) + (input + 3);
   }
 
   // Digit 11
@@ -122,11 +122,11 @@ static void RunProgram() {
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 5) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 5) * x);
+    // x = 1
+    output = (output * 26) + (input + 5);
   }
 
   // Digit 12
@@ -134,11 +134,11 @@ static void RunProgram() {
   x = (output % 26) + -5;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 11) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 11) * x);
+    // x = 1
+    output = (output * 26) + (input + 11);
   }
 
   // Digit 13
@@ -146,11 +146,11 @@ static void RunProgram() {
   x = (output % 26) + -9;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 12) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 12) * x);
+    // x = 1
+    output = (output * 26) + (input + 12);
   }
 
   // Digit 14
@@ -158,11 +158,11 @@ static void RunProgram() {
   x = (output % 26) + 0;
   output = output / 26; // SPECIAL INPUT CHAR WHERE OUTPUT GETS LOWERED BY "/ 26" SEEMINGLY?
   if (x == input) {
-    x = 0;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 0 so nothing happens
+    output = output;
   } else {
-    x = 1;
-    output = (output * ((25 * x) + 1)) + ((input + 10) * x);
+    // x = 1
+    output = (output * 26) + (input + 10);
   }
 }
 
